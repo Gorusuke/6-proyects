7431960197b932125975ef2ec2855a01b2fc7050
Jose Andres <Gorusuke123@gmail.com>
2021-05-21 23:59:54 -0500

    Iniciando el reto de 6 proyectos con vanilla js y empezamos con Countdown timer

---
+++ Countdown Timer/script.js
@@ -0,0 +1,32 @@
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+
+const newYears = "Jan 1 2022 00:00:00 GMT-0500";
+
+function countdown() {
+  const newYearDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysElement.innerHTML = days;
+  hoursElement.innerHTML = formatTime(hours);
+  minutesElement.innerHTML = formatTime(minutes);
+  secondsElement.innerHTML = formatTime(seconds);
+
+  console.info(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown(); // initial Call
+
+setInterval(countdown, 1000);
---
+++ README.md
@@ -0,0 +1,45 @@
+# 6 Projects to practice
+
+You can find the projects [here]().
+
+1. Countdown Timer
+
+-   Design: https://uidesigndaily.com/posts/sketch-countdown-timer-day-876
+-   future date
+-   timer
+
+2. Recipe App
+
+-   Design: https://uidesigndaily.com/posts/sketch-recipe-app-food-mobile-day-615
+-   recipe info on click
+-   fav recipe (w/ localStorage)
+
+3. Notes App
+
+-   Design: https://uidesigndaily.com/posts/photoshop-notes-widget-day-65
+-   CRUD note
+-   markdown
+-   localStorage
+
+4 ToDo App
+
+-   Design: http://todomvc.com/examples/react/#/
+-   CRUD todos
+-   localStorage
+
+5. Movie App
+
+-   Design: https://uidesigndaily.com/posts/photoshop-movie-app-mobile-day-193
+-   list of movies
+-   movie info
+-   fav movie
+-   actors
+
+
+6. Password Generator
+
+-   Design: https://codepen.io/FlorinPop17/full/BaBePej
+-   numbers
+-   letters
+-   symbols
+-   copy to clipboard
