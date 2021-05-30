0050c12116ac9684211848b999a3fa27a00dc8ce
Jose Andres <Gorusuke123@gmail.com>
2021-05-30 01:29:22 -0500

    avanzando con el Js del 3er proyecto. Tenemos cosillas por arreglar

--- Notes App/script.js
+++ Notes App/script.js
@@ -1,16 +1,55 @@
+const notesContaioner = document.getElementById("notes");
 const save = document.getElementById("save");
+const deleteNote = document.getElementById("delete");
+const textarea = document.getElementById("text");
 
 const saveContent = "Save";
 const editContent = "Edit";
 save.innerHTML = saveContent;
+// console.info();
 
 save.addEventListener("click", () => {
-  save.textContent === "Edit"
-    ? (save.innerHTML = saveContent)
-    : (save.innerHTML = editContent);
-  // if (save.textContent === "Edit") {
-  //   save.innerHTML = saveContent;
-  // } else {
-  //   save.innerHTML = editContent;
-  // }
+  if (save.textContent === "Edit") {
+    save.innerHTML = saveContent;
+    textarea.removeAttribute("readonly");
+    textarea.classList.remove("desibled");
+    editToLocalStorage(textarea.value);
+  } else {
+    save.innerHTML = editContent;
+    textarea.classList.add("desibled");
+    // console.info(textarea.value);
+    addToLocalStorage(textarea.value);
+    textarea.setAttribute("readonly", "");
+  }
+});
+
+function addToLocalStorage(note) {
+  const notes = getToLocalStorage();
+  localStorage.setItem("notes", JSON.stringify([...notes, note]));
+}
+
+function removeFromLocalStorage(textNote) {
+  const notes = getToLocalStorage();
+  localStorage.setItem(
+    "notes",
+    JSON.stringify(notes.filter((text) => text !== textNote))
+  );
+}
+
+function editToLocalStorage(textNote) {
+  const notes = getToLocalStorage();
+  localStorage.setItem(
+    "notes",
+    JSON.stringify(notes.filter((text) => text !== textNote))
+  );
+}
+
+function getToLocalStorage() {
+  const notes = JSON.parse(localStorage.getItem("notes"));
+  return notes === null ? [] : notes;
+}
+
+deleteNote.addEventListener("click", () => {
+  removeFromLocalStorage(textarea.value);
+  notesContaioner.classList.add("disabled");
 });
